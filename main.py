be9695d98ae0c696a07a291e1456968f065545ba
Alexandre Amory <alexandre.amory@santannapisa.it>
2022-03-06 18:44:09 +0100

    reading the task duration form dzn file

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import argparse
 import time
 import subprocess
 import json
+import pymzn
+
+# internal lib
+#from common import plot_gantt
 
 def main():
     """ Run Minizinc model and plot the resulting schedule 
@@ -28,6 +32,20 @@ def main():
         print ("ERROR: File %s not found" % (args.data))
         exit(1)
 
+    # build a duration matrix out of the DZN file
+    input_dict = pymzn.dzn2dict(str(args.data))
+    #print(input_dict)
+    n_jobs=input_dict["N_JOBS"]
+    n_cores=input_dict["N_CORES"]
+    d_list = []
+    i=0
+    for j in range(n_jobs):
+        temp_list=[]
+        for c in range(n_cores):
+            temp_list.append(input_dict["d"][i])
+            i = i+1
+        d_list.append(temp_list)
+    #print(d_list)
 
     path_model=args.model
     path_data=args.data
@@ -56,10 +74,32 @@ def main():
     out_json=b''.join(out_str).decode('utf-8')
     print("JSON output:")
     print(out_json)
-
+    # convert json string into a dictionary, easing data access
     out_dict=json.loads(out_json)
     print(out_dict)
+    print(out_dict["s"])
 
+    # the plotting tool uses yaml format, so we need another convertion
+    # yaml format: https://yatss.readthedocs.io/en/latest/#output-file-schedule-yaml-file
+    sched = {}
+    sched['title'] = 'Minizinc schedule'
+    sched['sched'] = []
+    for c in range(n_cores):
+        sched_task = {}
+        sched_task['name'] = "c"+str(c)
+        sched_task['color'] = 'blue'
+        sched_task['jobs'] = []
+        for j in range(n_jobs):
+            start_time=0
+            end_time=0
+            # if the job j has been assigned to core c, then get its start and end times
+            if out_dict["sel"][j][c] == 1:
+                start_time = out_dict["s"][j][c]
+                end_time = start_time + d_list[j][c]
+                sched_task['jobs'].append([start_time,end_time])
+        sched['sched'].append(sched_task)    
+
+    #plot_gantt(sched)
 
 if __name__ == "__main__":
     main()
