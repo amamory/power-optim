be8aafd74b31af9521f19751a5d7ce46b269b33d
Alexandre Amory <alexandre.amory@santannapisa.it>
2022-04-22 17:31:52 +0200

    new island mapping sequencer based on enumeration rather than tree

--- graph/load_sharing.py
+++ graph/load_sharing.py
@@ -8,35 +8,43 @@
 # https://docs.ray.io/en/latest/cluster/jobs-package-ref.html
 import sys
 import os
-import subprocess
 import multiprocessing as mp
 import networkx as nx
 import pandas as pd
 import numpy as np
 import math
 import time
-import yaml
-import random
 
-# libs
-import tree
-
-n_threads = 1
-n_islands = 3
-initial_task = 5
+n_threads = 3
+n_islands = 2
+n_tasks = 5
 n = 1
 while (n_threads > n_islands**n):
     n=n+1
-end_task = initial_task-n+1
-print (n, n_islands**n)
-task_placements = tree.task_island_combinations(n_islands,initial_task,end_task)
-for l in task_placements:
-    print (l.data)
-    for i in l.islands:
-        print (i)
-task_placements = [i.islands for i in task_placements]
-task_placements = [[[6,5],[],[]]]
-task_placements = [[[],[],[]]]
+
+# the size of the entire search space of 'n_tasks' placements onto 'n_islands'
+total_task_placements = n_islands**n_tasks
+# it represents how many work packages i can divide the entire search space (total_task_placements)
+# of placements considering a parallelism of 'n_threads'
+search_space_subdivisions = n_islands**n
+# the size of each work package, i.e., the number of placements to be checked by work package
+placements_per_workload = int (math.ceil(float(total_task_placements) / float(search_space_subdivisions)))
+print ('total_task_placements:',total_task_placements)
+print ('search_space_subdivisions:', search_space_subdivisions)
+print ('placements_per_workload:', placements_per_workload)
+
+# Generate the data structure sent to the pool of 'threads', i.e., the initial placement and 
+# how many placements each work package must check. 
+# The way it's calculated, all work package should have the same size, regardeless 
+# n_threads, n_islands, n_tasks
+placement_cnt = 0
+placement_setup_list = []
+while (placement_cnt < total_task_placements):
+    # initial placement id and the number of placements to be generated from this initial one
+    placement_setup_list.append((placement_cnt,placements_per_workload))
+    placement_cnt += placements_per_workload
+    
+print ('work packages:', placement_setup_list)
 
 # Check whether it is possible to prune an entire branch of the search space.
 # This might happen if the tasks placed so far are big enough to break the deadline or power constraints.
@@ -44,23 +52,37 @@ task_placements = [[[],[],[]]]
 def prune():
     pass
 
-def search_best_placement(intial_placement) -> list():
-    print (intial_placement, mp.current_process().name,mp.Process().name)
-    search_tree = tree.Tree(intial_placement,initial_task,n_islands)
-    n_leafs = 0
-    placement = search_tree.get_next_leaf()
-    while (placement):
+# converts an integer in to a task mapping, i.e., a list of list
+def get_mapping(curr_mapping, n_islands, n_tasks) -> list():
+    mapping = [[] for i in range(n_islands)]
+    island = 0
+    # compute island onto which I'm mapping the i-th task
+    for i in range(n_tasks): # scanning through all the n tasks
+        island = int(curr_mapping / (n_islands**(i)) % n_islands)
+        mapping[island].append(i)
+    return mapping
+
+# function to test whether the Pool mechanism and task mapping sequencer are generating 
+# all the possible task mappings
+def search_best_placement(placement_setup) -> list():
+    print ('starting work load:', placement_setup, mp.current_process().name,mp.Process().name)
+    current_placement = placement_setup[0]
+    n_placements = placement_setup[1]
+    # f = open(mp.Process().name+'.txt','w+')
+    # f.write(str(placement_setup)+'\n')
+    for i in range(n_placements):
+        placement = get_mapping(current_placement, n_islands, n_tasks)
         print (mp.current_process().name, placement)
-        #time.sleep(random.randrange(2, 10))
-        placement = search_tree.get_next_leaf()
-        n_leafs += 1
+        # f.write(str(placement)+'\n')
+        current_placement += 1
     print (mp.current_process().name,'done!')
-    return [n_leafs]
+    # f.close()
+    return []
 
 
 pool = mp.Pool(n_threads)
 best_placement_list = []
-for best_placement in pool.map(search_best_placement, task_placements):
+for best_placement in pool.map(search_best_placement, placement_setup_list):
     best_placement_list.append(best_placement)
 
 print (best_placement_list)
